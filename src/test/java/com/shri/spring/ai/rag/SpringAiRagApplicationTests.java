ead594c1563a5d361d8d1548392fdddde8f6d3ec
zeeroiq <shrikantvishwakarma@outlook.com>
2025-01-05 03:17:13 +0530

    spring-ai-rag project initialization

---
+++ src/test/java/com/shri/spring/ai/rag/SpringAiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shri.spring.ai.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
