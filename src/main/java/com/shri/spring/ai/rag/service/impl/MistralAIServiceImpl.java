7c60b7c4e40daaefc38e527f1bb708817d7f7032
zeeroiq <shrikantvishwakarma@outlook.com>
2025-01-05 21:52:41 +0530

    querying vector store. closes #4

--- src/main/java/com/shri/spring/ai/rag/service/impl/MistralAIServiceImpl.java
+++ src/main/java/com/shri/spring/ai/rag/service/impl/MistralAIServiceImpl.java
@@ -9,7 +9,15 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,11 +25,19 @@ import org.springframework.stereotype.Service;
 public class MistralAIServiceImpl implements AIService {
 
     private final ChatModel chatModel;
+    private final SimpleVectorStore vectorStore;
+
+    @Value("classpath:/templates/prompt-template-rag.st")
+    private Resource promptTemplateRag;
 
     @Override
     public Answer askQuestion(Question question) {
-        PromptTemplate promptTemplate = new PromptTemplate(question.question());
-        Prompt prompt = promptTemplate.create();
+        List<Document> documents = vectorStore.similaritySearch(SearchRequest.builder().query(question.question()).topK(4).build());
+        assert documents != null;
+        List<String> contentList = documents.stream().map(Document::getText).toList();
+        PromptTemplate promptTemplate = new PromptTemplate(promptTemplateRag);
+        Prompt prompt = promptTemplate.create(Map.of("input", question.question(), "document", String.join("\n", contentList)));
+        contentList.forEach(log::info);
         ChatResponse response = chatModel.call(prompt);
         log.info("Response: {}", response.getResult().getOutput().getContent());
         return new Answer(response.getResult().getOutput().getContent());
