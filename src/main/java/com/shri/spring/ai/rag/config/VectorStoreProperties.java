755ff0e2b3d9b6f385b6d1116179c3bd623d5558
zeeroiq <shrikantvishwakarma@outlook.com>
2025-01-05 20:13:10 +0530

    data load to vector store. closes #2

--- src/main/java/com/shri/spring/ai/rag/config/VectorStoreProperties.java
+++ src/main/java/com/shri/spring/ai/rag/config/VectorStoreProperties.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +16,5 @@ import org.springframework.context.annotation.Configuration;
 public class VectorStoreProperties {
 
     private String vectorStoreDir;
+    private List<Resource> document2load;
 }
